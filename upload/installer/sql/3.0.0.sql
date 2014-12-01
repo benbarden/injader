e9a40bae20163f061864359cd89c97468d4c4a44
Ben Barden <github@benbarden.com>
2014-12-01 21:13:58 +0000

    Updating version for completeness

--- upload/installer/sql/3.0.0.sql
+++ upload/installer/sql/3.0.0.sql
@@ -6,3 +6,6 @@ DROP TABLE IF EXISTS maj_ratings;
 UPDATE maj_sys_preferences SET content = 'injader' WHERE preference = 'prefDefaultTheme';
 
 ALTER TABLE maj_areas DROP COLUMN nav_type, DROP INDEX nav_type;
+
+UPDATE maj_sys_preferences SET content = '3.0.0' WHERE preference = 'prefCMSVersion';
+
